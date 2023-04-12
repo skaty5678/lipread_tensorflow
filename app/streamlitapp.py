f436f7c6977c8d86405290dcc4f0e9f9f6ca6987
skaty5678 <skaty5678@gmail.com>
2023-04-13 00:24:34 +0530

    commented the code

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -1,4 +1,4 @@
-#import all the dependencies
+# import necessary dependencies
 import streamlit as st
 import os
 import imageio
@@ -7,59 +7,63 @@ import tensorflow as tf
 from utils import load_data, num_to_char
 from modelutil import load_model
 
-#set the layout to the streamlit app as wide
+# set the layout of the Streamlit app to wide
 st.set_page_config(layout='wide')
 
-
-#setup the sidebar
+# set up the sidebar
 with st.sidebar:
+    # display logo and app title
     st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
     st.title('LipBuddy')
-    st.info('this application is originally developed from the lipnet deep learning model')
+    st.info('This application is originally developed from the LipNet deep learning model')
 
-
+# display app title
 st.title('LipNet full stack app')
-#generating a list of options or videos
+
+# generate a list of options for videos
 options = os.listdir(os.path.join('..','data','s1'))
 selected_video = st.selectbox('Choose Video',options)
 
+# set up two columns for displaying the video and model output
 col1, col2 = st.columns(2)
 
 if options:
-    # Rendering the video 
+    # render the selected video
     with col1:
         st.info('The video below displays the converted video in mp4 format')
+        # set up file paths for input and output videos
         file_path = os.path.join('..','data','s1', selected_video)
         output_path = 'test_video.mp4'
+        # convert video to mp4 format using ffmpeg
         cmd = f'ffmpeg -i {file_path} -vcodec libx264 {output_path} -y'
         status = os.system(cmd)
         if status == 0 and os.path.exists(output_path):
-            # Rendering inside of the app
+            # display the converted video inside the app
             video = open(output_path, 'rb') 
             video_bytes = video.read() 
             st.video(video_bytes)
         else:
+            # display an error message if video conversion fails
             st.error(f'Error: Failed to convert video. Status code: {status}')
 
-
+    # display the video as input to the model
     with col2:
-        st.info('this is all the machine learning model sees when making a prediction')
+        st.info('This is all the machine learning model sees when making a prediction')
+        # load the video and its annotations
         video, annotations = load_data(tf.convert_to_tensor(file_path))
+        # convert the video to a GIF for display
         imageio.mimsave('Animation.gif',video,fps=10)
         st.image('Animation.gif',width=400)
-        
-        #this is the output of the machine learning model as 
-        st.info('this is output of the machine learning as tokens')
+
+        # display the output of the model as tokens
+        st.info('This is the output of the machine learning as tokens')
         model = load_model()
+        # predict the text from the video
         yhat = model.predict(tf.expand_dims(video, axis=0))
         decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
         st.text(decoder)
 
-        #convert prediction to text 
-        st.info('decode the raw tokens into words')
+        # convert the model output from tokens to text
+        st.info('This is the decoded text')
         converted_preds = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
         st.text(converted_preds)
-
-
-
-
--- app/utils.py
+++ app/utils.py
@@ -3,46 +3,75 @@ from typing import List
 import cv2
 import os 
 
+# Define the vocabulary for the speech recognition model
 vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
+
+# Create a mapping from characters to integer ids
 char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
-# Mapping integers back to original characters
+
+# Create a mapping from integer ids back to original characters
 num_to_char = tf.keras.layers.StringLookup(
     vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
 )
 
 def load_video(path:str) -> List[float]: 
-    #print(path)
+    # Open the video at the specified path
     cap = cv2.VideoCapture(path)
+    
+    # Initialize a list to hold the video frames
     frames = []
+    
+    # Loop through each frame in the video
     for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))): 
         ret, frame = cap.read()
+        
+        # Convert the RGB frame to grayscale
         frame = tf.image.rgb_to_grayscale(frame)
+        
+        # Crop the frame to a specific size
         frames.append(frame[190:236,80:220,:])
+    
+    # Release the video object
     cap.release()
     
+    # Calculate the mean and standard deviation of the frames
     mean = tf.math.reduce_mean(frames)
     std = tf.math.reduce_std(tf.cast(frames, tf.float32))
+    
+    # Normalize the frames by subtracting the mean and dividing by the standard deviation
     return tf.cast((frames - mean), tf.float32) / std
     
 def load_alignments(path:str) -> List[str]: 
-    #print(path)
+    # Open the alignment file at the specified path
     with open(path, 'r') as f: 
         lines = f.readlines() 
+    
+    # Initialize a list to hold the speech tokens
     tokens = []
+    
+    # Loop through each line in the alignment file
     for line in lines:
         line = line.split()
         if line[2] != 'sil': 
             tokens = [*tokens,' ',line[2]]
+    
+    # Convert the speech tokens to integer ids using the character-to-integer mapping
     return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
 
 def load_data(path: str): 
+    # Convert the input path from bytes to string
     path = bytes.decode(path.numpy())
+    
+    # Extract the file name from the input path
     file_name = path.split('/')[-1].split('.')[0]
-    # File name splitting for windows
-    # file_name = path.split('\\')[-1].split('.')[0]
+    
+    # Create paths to the video file and alignment file based on the file name
     video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
     alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
+    
+    # Load the video frames and speech alignments
     frames = load_video(video_path) 
     alignments = load_alignments(alignment_path)
     
+    # Return the frames and alignments as a tuple
     return frames, alignments
